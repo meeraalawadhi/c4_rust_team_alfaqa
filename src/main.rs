60e0cbae3a7921bf7b55cbe2421186c62cf6bd35
Hamda <hamdahalm@hotmail.com>
2025-05-01 16:54:30 +0500

    Added support for branch statements

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
-// src/main.rs
 mod c4;
 
-use c4::{Parser, Expr};
+use c4::Parser;
 use std::env;
 use std::fs;
 
 fn main() -> Result<(), String> {
- let args: Vec<String> = env::args().collect();
- if args.len() < 2 {
- return Err(format!("Usage: {} <source.c>", args[0]));
- }
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        return Err(format!("Usage: {} <source.c>", args[0]));
+    }
 
- let input = fs::read_to_string(&args[1]).map_err(|e| format!("Failed to read {}: {}", args[1], e))?;
+    let input = fs::read_to_string(&args[1]).map_err(|e| format!("Failed to read {}: {}", args[1], e))?;
 
- let mut parser = Parser::new(&input);
- let expr = parser.parse_expr()?;
- println!("{:#?}", expr);
+    let mut parser = Parser::new(&input);
+    let stmts = parser.parse_block()?;
+    for stmt in stmts {
+        println!("{:#?}", stmt);
+    }
 
- Ok(())
+    Ok(())
 }
