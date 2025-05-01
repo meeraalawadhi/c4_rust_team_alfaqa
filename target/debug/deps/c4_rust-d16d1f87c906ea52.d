e36cfe287c56d2144ff6e53a923b1fea35f3374c
Hamda <hamdahalm@hotmail.com>
2025-05-01 20:25:08 +0500

    Added Rust Style Comments

--- target/debug/deps/c4_rust-d16d1f87c906ea52.d
+++
@@ -1,5 +0,0 @@
-D:\c4_rust_team_alfaqa\target\debug\deps\c4_rust-d16d1f87c906ea52.exe: src\main.rs
-
-D:\c4_rust_team_alfaqa\target\debug\deps\c4_rust-d16d1f87c906ea52.d: src\main.rs
-
-src\main.rs:
